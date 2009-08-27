f14fc0b6c3908c426607b43a60120a05abf7d8ed
Riccardo Narduzzi <riccardonar@gmail.com>
2009-08-27 08:51:08 +0000

    ora è possibile dichiare il template all'interno di un foreach. le funzioni vengono riconoscite in base al nome del template

--- plugins/attrs/compilable/ATalAttrCompilablePlugin_call.php
+++ plugins/attrs/compilable/ATalAttrCompilablePlugin_call.php
@@ -1,13 +1,14 @@
 <?php
 class ATalAttrCompilablePlugin_call extends ATalAttrCompilablePlugin{
 	function start(ATal_XMLDomElement $node, $attValue){
-		$pi = $this->dom->createProcessingInstruction("php",self::prepareCode($attValue));
+		$pi = $this->dom->createProcessingInstruction("php",self::prepareCode($attValue, $this->compiler));
 		$node->removeChilds();
 		$node->appendChild($pi);
 
 	}
-	public static  function prepareCode($attValue){
-		return " \$__tal->addScope(get_defined_vars()); __atal_template_{$attValue} (\$__tal ); \$__tal->removeScope();";
+	public static  function prepareCode($attValue, $compiler){
+		$nome = $attValue.md5($compiler->getTemplate());
+		return " \$__tal->addScope(get_defined_vars()); __atal_template_{$nome} (\$__tal ); \$__tal->removeScope();";
 	}
 
 }
--- plugins/attrs/compilable/ATalAttrCompilablePlugin_template.php
+++ plugins/attrs/compilable/ATalAttrCompilablePlugin_template.php
@@ -1,16 +1,17 @@
 <?php
 class ATalAttrCompilablePlugin_template extends ATalAttrCompilablePlugin{
 	function start(ATal_XMLDomElement $node, $attValue){
+		$nome = $attValue.md5($this->compiler->getTemplate());
 		$piStart = $this->dom->createProcessingInstruction("php",
-		" function __atal_template_".$attValue." (\$__tal ){ ".
+		" if (!function_exists('__atal_template_".$nome."')){ function __atal_template_".$nome." (\$__tal ){ ".
 		" extract(\$__tal->getData()); ");
 		$node->parentNode->insertBefore($piStart, $node);
 
-		$piEnd = $this->dom->createProcessingInstruction("php"," } ");
+		$piEnd = $this->dom->createProcessingInstruction("php"," }} ");
 		$node->parentNode->insertAfter($piEnd, $node);
 
 		if($node->hasAttributeNS(ATal::NS, 'call') && $node->getAttributeNS(ATal::NS,"call") == $attValue){
-			$pi = $this->dom->createProcessingInstruction("php",ATalAttrCompilablePlugin_call::prepareCode($attValue));		
+			$pi = $this->dom->createProcessingInstruction("php",ATalAttrCompilablePlugin_call::prepareCode($attValue, $this->compiler));		
 			$node->parentNode->insertAfter($pi, $piEnd);
 			try {
 				$node->removeAttributeNS(ATal::NS,"call");
