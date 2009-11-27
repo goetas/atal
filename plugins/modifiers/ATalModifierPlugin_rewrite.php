3e38979f3d8e9e3b006bd7811c5f3793f98f5a83
Riccardo Narduzzi <riccardonar@gmail.com>
2009-11-27 11:53:27 +0000

    modrewrite e modifica link del modulo Web

--- plugins/modifiers/ATalModifierPlugin_rewrite.php
+++ plugins/modifiers/ATalModifierPlugin_rewrite.php
@@ -4,18 +4,21 @@ class ATalModifierPlugin_rewrite extends ATalModifierPlugin {
 		return self::rewrite($str, $params);
 	}
 	public static function rewrite($url, array $params=array()){
-		$p = explode('/', $url);
+		list($modulo, $metodo) = explode('/', $url);
+		$modulo = strtolower($modulo);
 		
-		if ($params['id']){
-			$params['id'] = "_{$params['id']}";
-		}
-		if ($params['keys']){
-			$params['keys'] = preg_replace('/[^a-z0-9_]+/i', '-', iconv("UTF-8", "ASCII//TRANSLIT", $params['keys']));
+		if ($modulo == 'web'){
+			return "{$metodo}.html";
 		} else {
-			$params['keys'] = $p[0];
+			if ($params['id']){
+				$params['id'] = "_{$params['id']}";
+			}
+			if ($params['keys']){
+				$params['keys'] = preg_replace('/[^a-z0-9_]+/i', '-', iconv("UTF-8", "ASCII//TRANSLIT", $params['keys']));
+			} else {
+				$params['keys'] = $modulo;
+			}
+			return "{$modulo}/{$params['keys']}/{$metodo}{$params['id']}.html";
 		}
-		
-		return "{$p[0]}/{$params['keys']}/{$p[1]}{$params['id']}.html";
 	}
 }
-?>
