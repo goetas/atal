517d7d79e0653d6eebe757089ec4714ec1565ee2
Asmir Mustafic <goetas@lignano.it>
2009-12-02 15:42:25 +0000

    sistemato bug nel selettore css

--- selectors/ATalCssSelector.php
+++ selectors/ATalCssSelector.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'QueryPath'.DIRECTORY_SEPARATOR.'QueryPath.php';
 class ATalCssSelector extends ATalSelector {
 	public function select($query){
-		return qp($this->dom,$query);
+		return qp($this->dom,$query)->get();
 	}
 }
 
