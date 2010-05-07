764d600c8471702c2d1d65375856cb3f97f4769d
Asmir Mustafic <goetas@lignano.it>
2010-05-07 12:34:18 +0000

    migliorato il modificatore "preg_replace"

--- plugins/modifiers/ATalModifierPlugin_preg_replace.php
+++ plugins/modifiers/ATalModifierPlugin_preg_replace.php
@@ -1,7 +1,7 @@
 <?php
 class ATalModifierPlugin_preg_replace extends ATalModifierPlugin {
 	function modify($value, array $params = array()) {
-		return preg_replace( $params [0], $params [1], $value );
+		return preg_replace( $params [0], (string)$params [1], $value );
 	}
 }
 
