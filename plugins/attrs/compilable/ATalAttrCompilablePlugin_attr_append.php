4bc14adc912347e0279a137ddea5581271f63ad6
Asmir Mustafic <goetas@lignano.it>
2010-05-31 12:21:01 +0000

    fix

--- plugins/attrs/compilable/ATalAttrCompilablePlugin_attr_append.php
+++ plugins/attrs/compilable/ATalAttrCompilablePlugin_attr_append.php
@@ -16,7 +16,7 @@ class ATalAttrCompilablePlugin_attr_append extends ATalAttrCompilablePlugin_attr
 				$attVal = $node->getAttribute( $attName );
 
 				if(preg_match( $regex, $attVal )){
-					$precode = $varName . "['$attName']=(isset({$varName}['$attName']))?{$varName}['$attName']:" . preg_replace( $regex, "\\1", $attVal ) . ";\n";
+					$precode = $varName . "['$attName']=(isset({$varName}['$attName']))?{$varName}['$attName']:\"" . addcslashes(preg_replace( $regex, "{\\1}", $attVal ),"\"\\") . "\";\n";
 				}else{
 					$precode .= $varName . "['$attName']=(isset({$varName}['$attName']))?{$varName}['$attName']:'" . addcslashes( $node->getAttribute( $attName ), "'" ) . "';\n";
 				}
