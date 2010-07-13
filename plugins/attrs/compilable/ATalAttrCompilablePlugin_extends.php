6bb9f854c14a1815d7e74c7c92c701581944b737
Asmir Mustafic <goetas@lignano.it>
2010-07-13 14:01:27 +0000

    ora l'estensione dei template funziona bene e permette l'estensione multipla dei template

--- plugins/attrs/compilable/ATalAttrCompilablePlugin_extends.php
+++ plugins/attrs/compilable/ATalAttrCompilablePlugin_extends.php
@@ -1,13 +1,21 @@
 <?php
 class ATalAttrCompilablePlugin_extends extends ATalAttrCompilablePlugin{
 	function start(ATal_XMLDomElement $node, $attValue){
-		$olddir = getcwd();
-		$dir = dirname($this->compiler->getTemplate());
-		chdir($dir);
 
+		$dir = dirname($this->compiler->getTemplate());
+
+		$this->scanTemplates($dir, $node, $attValue);
+
+		return self::STOP_NODE | self::STOP_ATTRIBUTE;
+	}
+	protected function scanTemplates($dir , ATal_XMLDomElement $node, $attValue, &$blocksNodes = array()){
+		$a =  array("t" => ATal::NS ) ;
+
+		$olddir = getcwd();
+		chdir($dir);
 		list ($tpl,$tipo,$query ) = $this->compiler->parseUriParts($attValue);
 		list($xmlTemplate,$tplDom) =  $this->compiler->loadXMLTemplate($tpl,$tipo,$query );
-		chdir($olddir);
+
 
 
 		//$xmlTemplate continene il template originale
@@ -16,19 +24,28 @@ class ATalAttrCompilablePlugin_extends extends ATalAttrCompilablePlugin{
 
 		$padre = $node->parentNode;
 		$dom = $node->ownerDocument;
+		//echo htmlspecialchars($xmlTemplate->saveXML()),"<hr/>";
 
-		$padre->removeChild($node);
-		$a =  array("t" => ATal::NS ) ;
-		// soltituisco il tag "extends" con il contenuto del template a cui si rifferisce
-		foreach ( $xmlTemplate->query( "/t:atal-content/node()|/text()",$a) as $nodeNew ){
-			$padre->appendChild($dom->importNode($nodeNew,true));
+		foreach ( $node->query( "t:block", $a ) as $templateDettagliato ){
+			$name = $templateDettagliato->getAttribute("name");
+			$blocksNodes[$name] = $templateDettagliato;
 		}
 
+		$padre->removeChild($node);
 
-
+		// soltituisco il tag "extends" con il contenuto del template a cui si rifferisce
+		foreach ( $xmlTemplate->query( "/t:atal-content/node()|/text()",$a) as $nodeNew ){
+			$nd = $dom->importNode($nodeNew,true);
+			$padre->appendChild($nd);
+			if($nd instanceof ATal_XMLDomElement && $nd->getAttributeNS(ATal::NS, 'extends')){
+				$this->scanTemplates(dirname($tpl) , $nd, $nd->getAttributeNS(ATal::NS, 'extends'), $blocksNodes );
+			}
+		}
+		chdir($olddir);
 
 		// cerco eventuali template dettagliati da sostituire a quelli di default
-		foreach ( $node->query( "t:block", $a ) as $templateDettagliato ){
+
+		foreach ( $blocksNodes as $templateDettagliato ){
 			$nomeTemplate = $templateDettagliato->getAttribute("name");
 			$sostituisci = $templateDettagliato->getAttribute("replace")=="true";
 
@@ -44,21 +61,18 @@ class ATalAttrCompilablePlugin_extends extends ATalAttrCompilablePlugin{
 						$ref = $nd;
 					}
 					$nodoDaSostituire->parentNode->removeChild($nodoDaSostituire);
-					$this->compiler->applyTemplats($nodoDaSostituire->parentNode);
 				}else{
 					$nodoDaSostituire->removeChilds();
 					foreach ($templateDettagliato->childNodes as $n){
 						$nd  = $dom->importNode($n->cloneNode(1), true);
 						$nodoDaSostituire->appendChild($nd);
 					}
-					$this->compiler->applyTemplats($nodoDaSostituire);
 				}
 
 			}
 		}
 
-
-		return self::STOP_NODE | self::STOP_ATTRIBUTE;
+		$this->compiler->applyTemplats($padre);
 	}
 
 
