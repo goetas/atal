8561ee4504293e740b78a597d5364051f9b5ef42
Asmir Mustafic <goetas@lignano.it>
2010-11-25 08:01:49 +0000

    atal!

--- autoload.php
+++ autoload.php
@@ -2,7 +2,6 @@
 spl_autoload_register (function($cname){
 	$ns = "goetas\\atal\\";
 	if(strpos($cname,$ns)!==false){
-		//echo $cname." autoload<hr/>";
 		$path = __DIR__.DIRECTORY_SEPARATOR.str_replace("\\",DIRECTORY_SEPARATOR,substr($cname,strlen($ns))).".php";
 		if(is_file($path)){
 			require_once($path);
