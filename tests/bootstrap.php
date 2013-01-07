b8d49080e9c1d8d4ea2987c0387a611beb27bbf3
Asmir Mustafic <goetas@lignano.it>
2013-01-07 09:31:39 +0000

    fix boostrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,22 @@
 <?php 
-require __DIR__."/../src/autoload.php";
+
 require __DIR__."/../vendors/pluginsys/src/autoload.php";
-require __DIR__."/../vendors/xmldom/src/autoload.php";
+require __DIR__."/../src/autoload.php";
+
+
+
+//psr-0 autoloader
+foreach(array(
+		"goetas\\xml\\"=>__DIR__."/../vendors/xmldom/src/",
+		//"goetas\\atal\\"=>__DIR__."/../src/",
+		) as $ns => $dir){
+
+	spl_autoload_register ( function($cname)use($ns, $dir){
+		if(strpos($cname,$ns)===0){
+			$path = $dir.strtr($cname, "\\","/").".php";
+			require_once ($path);
+		}
+	});
+}
+
+	
