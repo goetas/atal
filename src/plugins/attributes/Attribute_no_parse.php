5f79b8e23298c1f9c55a5b90fca567cccd319992
Asmir Mustafic <goetas@lignano.it>
2013-03-01 08:25:46 +0000

    i t:id non esistono piu

--- src/plugins/attributes/Attribute_no_parse.php
+++ src/plugins/attributes/Attribute_no_parse.php
@@ -5,9 +5,6 @@ use goetas\atal\ATal;
 use goetas\atal\Attribute;
 class Attribute_no_parse extends Attribute {
 	function start(xml\XMLDomElement $node, \DOMAttr $att) {
-		foreach ( $node->query( ".//*[@t:id]/@t:id", array("t" => ATal::NS ) ) as $tt ){
-			$tt->ownerElement->removeAttributeNode( $tt );
-		}
 		return self::STOP_NODE;
 	}
 
