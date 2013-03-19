c8241d7e5a07e03d91439fe17da3f0a09366bc98
Asmir Mustafic <goetas@lignano.it>
2013-03-19 14:11:18 +0000

    aggiunto modificatore "prepend if"

---
+++ src/plugins/modifiers/Modifier_prepend_if.php
@@ -0,0 +1,9 @@
+<?php
+namespace goetas\atal\plugins\modifiers;
+use goetas\atal\Modifier;
+class Modifier_prepend_if extends Modifier{
+	function modify($str, array $params=array()){
+		
+		return $str.((!array_key_exists(1,$params) || $params[1] )?$params[0]:"");
+	}
+}
