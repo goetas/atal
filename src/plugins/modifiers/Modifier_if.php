02e8c4b004d583a281fc22d5b8c129d29e749620
Asmir Mustafic <goetas@lignano.it>
2013-03-19 14:17:47 +0000

    prepend if e if modificatori

---
+++ src/plugins/modifiers/Modifier_if.php
@@ -0,0 +1,8 @@
+<?php
+namespace goetas\atal\plugins\modifiers;
+use goetas\atal\Modifier;
+class Modifier_if extends Modifier{
+	function modify($str, array $params=array()){
+		return ((!array_key_exists(0,$params) || $params[0] )?$str:$params[1]).$str;
+	}
+}
--- src/plugins/modifiers/Modifier_prepend_if.php
+++ src/plugins/modifiers/Modifier_prepend_if.php
@@ -3,7 +3,6 @@ namespace goetas\atal\plugins\modifiers;
 use goetas\atal\Modifier;
 class Modifier_prepend_if extends Modifier{
 	function modify($str, array $params=array()){
-		
-		return ((!array_key_exists(1,$params) || $params[1] )?$params[0]:"").$str;
+		return ($str && (!array_key_exists(1,$params) || $params[1] )?$params[0]:"").$str;
 	}
 }
