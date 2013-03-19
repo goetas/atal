33ec701596810db2372a21e19aa736f59778f632
Asmir Mustafic <goetas@lignano.it>
2013-03-19 14:23:52 +0000

    fix if modifier

--- src/plugins/modifiers/Modifier_if.php
+++ src/plugins/modifiers/Modifier_if.php
@@ -3,6 +3,6 @@ namespace goetas\atal\plugins\modifiers;
 use goetas\atal\Modifier;
 class Modifier_if extends Modifier{
 	function modify($str, array $params=array()){
-		return ((!array_key_exists(0,$params) || $params[0] )?$str:$params[1]).$str;
+		return ((!array_key_exists(0,$params) || $params[0] )?$str:$params[1]);
 	}
 }
